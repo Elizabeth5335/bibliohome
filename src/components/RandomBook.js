239457b876c6f3e44d51848f640d65bc52149472
Elizabeth5335 <51746623+Elizabeth5335@users.noreply.github.com>
2024-07-27 17:41:53 +0200

    added randomBook

--- src/components/RandomBook.js
+++ src/components/RandomBook.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { useBooks } from "../context/BooksContext";
+import BookCard from "./BookCard";
 
 export default function RandomBook() {
+  const { books } = useBooks();
+  const [randomBook, setRandomBook] = React.useState(null);
+
+  function generateRND(isForAdults) {
+    if (books) {
+      // Filter books based on isForAdults
+      const filteredBooks = Object.values(books).filter((book) => book.forAdults === isForAdults);
+
+      if (filteredBooks.length > 0) {
+        const randomIndex = Math.floor(Math.random() * filteredBooks.length);
+        setRandomBook(filteredBooks[randomIndex]);
+      } else {
+        setRandomBook(null);
+      }
+    }
+  }
+
   return (
-      <section>
-      <h2>Перевір свою удачу!</h2> 
+    <section>
+      <h2>Перевір свою удачу!</h2>
       <p>Випадкова книга</p>
-      <button>Згенерувати</button>
-      </section>
+      <button onClick={() => generateRND(true)}>Згенерувати дорослому</button>
+      <button onClick={() => generateRND()}>Згенерувати дитині</button>
+      {randomBook && (
+
+        <div className="random-book">
+          <h3>{randomBook.name}</h3>
+          <p><b>Автор:</b> {randomBook.author}</p>
+          <p><b>Категорія:</b> {randomBook.category}</p>
+          <p><b>Вартість прокату:</b> {randomBook.price} zł</p>
+          {randomBook.url && (
+            <img
+              className="bookCover"
+              src={randomBook.url}
+              alt={randomBook.name}
+              style={{ maxWidth: "200px", maxHeight: "300px" }}
+            />
+          )}
+        </div>
+      )}
+    </section>
   );
 }
